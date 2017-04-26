ff6ab2eb96cf5619bbadf0ca46de464fbefbc536
Cntt32 <Admin32@Cntt32s-iMac.local>
2017-04-26 16:27:55 +0700

    abc

---
+++ IOS-SWIFT3-GST2017-FlickrUICollectionView/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  IOS-SWIFT3-GST2017-FlickrUICollectionView
+//
+//  Created by Cntt32 on 4/26/17.
+//  Copyright © 2017 com.group08. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
